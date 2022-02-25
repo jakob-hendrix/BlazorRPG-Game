f7007f679866376ffaaa4b54c03b915b54608c6c
Jakob Hendrix <jhendrix001@gmail.com>
2022-02-25 16:31:16 -0600

    2.2 Added GameSession VM and tests

---
+++ simple-rpg-game/SimpleRPG.Game.Engine.Tests/ViewModels/GameSessionTests.cs
@@ -0,0 +1,22 @@
+﻿using SimpleRPG.Game.Engine.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SimpleRPG.Game.Engine.Tests.ViewModels
+{
+    public class GameSessionTests
+    {
+        [Fact]
+        public void CreateSimpleGameSession()
+        {
+            var sut = new GameSession();
+
+            Assert.Equal("Anomen",sut.CurrentPlayer.Name);
+            Assert.Equal(1000,sut.CurrentPlayer.Gold);
+        }
+    }
+}
---
+++ simple-rpg-game/SimpleRPG.Game.Engine/ViewModels/GameSession.cs
@@ -0,0 +1,18 @@
+﻿using SimpleRPG.Game.Engine.Models;
+
+namespace SimpleRPG.Game.Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player
+            {
+                Name = "Anomen",
+                Gold = 1000
+            };
+        }
+    }
+}
