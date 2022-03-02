6fe711e88e9089e722a429726dcd61b37c4c5869
Jakob Hendrix <jhendrix001@gmail.com>
2022-03-02 16:40:47 -0600

    3.4 - added dice roller service
    Implemented the author's library - D20Tek die roller

---
+++ simple-rpg-game/SimpleRPG.Game.Engine/Services/DiceService.cs
@@ -0,0 +1,49 @@
+﻿using D20Tek.DiceNotation;
+using D20Tek.DiceNotation.DieRoller;
+using System;
+
+namespace SimpleRPG.Game.Engine.Services
+{
+    public class DiceService : IDiceService
+    {
+        private static readonly IDiceService _instance = new DiceService();
+
+        /// <summary>
+        /// Make constructor private to implement singletone pattern.
+        /// </summary>
+        private DiceService()
+        {
+        }
+
+        /// <summary>
+        /// Static singleton property
+        /// </summary>
+        public static IDiceService Instance => _instance;
+
+        //--- IDiceService implementation
+
+        public IDice Dice { get; } = new Dice();
+        public IDieRoller DieRoller { get; private set; } = new RandomDieRoller();
+
+        public IDiceConfiguration Configuration => Dice.Configuration;
+
+        public IDieRollTracker? RollTracker { get; private set; } = null;
+
+        public void Configure(IDiceService.RollerType rollerType, bool enableTracker = false)
+        {
+            RollTracker = enableTracker ? new DieRollTracker() : null;
+
+            // NOTE: cool new switch assigment syntax
+            DieRoller = rollerType switch
+            {
+                IDiceService.RollerType.Random => new RandomDieRoller(RollTracker),
+                IDiceService.RollerType.Crypto => new CryptoDieRoller(RollTracker),
+                IDiceService.RollerType.MathNet => new MathNetDieRoller(RollTracker),
+                _ => throw new ArgumentOutOfRangeException(nameof(rollerType))
+            };
+        }
+
+        public DiceResult Roll() => Dice.Roll(DieRoller);
+        public DiceResult Roll(string diceNotation) => Dice.Roll(diceNotation, DieRoller);
+    }
+}
---
+++ simple-rpg-game/SimpleRPG.Game.Engine/Services/IDiceService.cs
@@ -0,0 +1,22 @@
+﻿using D20Tek.DiceNotation;
+using D20Tek.DiceNotation.DieRoller;
+
+namespace SimpleRPG.Game.Engine.Services
+{
+    public interface IDiceService
+    {
+        public enum RollerType
+        {
+            Random = 0,
+            Crypto = 1,
+            MathNet = 2
+        }
+
+        IDice Dice { get; }
+        IDiceConfiguration Configuration { get; }
+        IDieRollTracker? RollTracker { get; }
+        void Configure(RollerType rollerType, bool enableTracker = false);
+        DiceResult Roll();
+        DiceResult Roll(string diceNotation);
+    }
+}
